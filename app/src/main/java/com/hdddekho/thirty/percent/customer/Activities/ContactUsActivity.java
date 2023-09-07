cdabec105288843e2bda92789fc368cf6b6d29ad
devapp-nishant <nishant8581mishra@gmail.com>
2023-09-08 00:02:17 +0530

    CODE TO MAKE ECOMMERCE APP IN ANDROID

---
+++ app/src/main/java/com/hdddekho/thirty/percent/customer/Activities/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package com.hdddekho.thirty.percent.customer.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hdddekho.thirty.percent.customer.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
